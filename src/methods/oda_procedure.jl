6737d5255847e96efbb8e11d4412c72a3652f9d6
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-04-11 14:26:27 +0200

    Correction oda_procedure to work with Double64

--- src/methods/oda_procedure.jl
+++ src/methods/oda_procedure.jl
@@ -6,10 +6,12 @@ function find_minima_oda(energy_kin0::Real, energy_kin1::Real,
                          energy_har01::Real, energy_har10::Real,
                          D0::AbstractArray{<:Real}, D1::AbstractArray{<:Real}, tmpD::AbstractArray{<:Real}, 
                          model::AbstractDFTModel, discretization::KohnShamDiscretization)
+    
+    T = discretization.elT
 
     if isthereExchangeCorrelation(model)
         # DEFINE THE OBJECTIV FUNCTION
-        function f(t::Float64)        
+        function f(t)        
             @. tmpD = t*D0 .+ (1-t) * D1
             (t * (energy_kin0 + energy_cou0) + (1-t) * (energy_kin1 + energy_cou1) + 
             t^2*energy_har0 + (1-t)^2 * energy_har1 + t*(1-t) * (energy_har01 + energy_har10) +
@@ -17,18 +19,18 @@ function find_minima_oda(energy_kin0::Real, energy_kin1::Real,
         end
 
         # PERFORM THE OPTIMISATION THROUGHT THE GOLDEN SECTION's METHOD
-        res = optimize(f, 0.0, 1.0, GoldenSection())
+        res = optimize(f, zero(T), one(T), GoldenSection())
 
         return res.minimizer, res.minimum
     else
         # DEFINE THE OBJECTIV FUNCTION
-        function f2(t::Float64)        
+        function f2(t)        
             t * (energy_kin0 + energy_cou0) + (1-t) * (energy_kin1 + energy_cou1) + 
             t^2*energy_har0 + (1-t)^2 * energy_har1 + t*(1-t) * (energy_har01 + energy_har10)
         end
 
         # PERFORM THE OPTIMISATION THROUGHT THE GOLDEN SECTION's METHOD
-        res = optimize(f2, 0.0, 1.0, GoldenSection())
+        res = optimize(f2, zero(T), one(T), GoldenSection())
 
         return res.minimizer, res.minimum
     end
