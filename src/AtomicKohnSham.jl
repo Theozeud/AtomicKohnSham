4bbe2e13c7b6f11533e04190e357c719b81e669e
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-06-01 20:54:21 +0200

    Update AtomicKohnSham.jl

--- src/AtomicKohnSham.jl
+++ src/AtomicKohnSham.jl
@@ -25,22 +25,18 @@ module AtomicKohnSham
 
 
     
-    # ANNEXE
-    include("utils.jl")
-    include("maths.jl")
+    # -------------------------------------
+    #               UTILS
+    # -------------------------------------
+    include("utils/free allocation.jl")
+    include("utils/sparse tools.jl")
 
-    # MESH
-    export Mesh, linmesh, geometricmesh, polynomialmesh, expmesh
-    include("mesh.jl")
-
-
+    
     # -------------------------------------
     #               POLYSET
     # -------------------------------------
     # ALL THESE TOOLS SHOULD MOVE TO A NEW PACKAGE POLYSET
-    
     export PolySet, Legendre, IntLegendre
-
     export evaluate!, integrate!, allocate_polyset, mul, npolys, maxdeg
 
     include("fem/polynomial.jl")
@@ -48,10 +44,9 @@ module AtomicKohnSham
 
 
     # -------------------------------------
-    #               1D FEM TOOLS
+    #               FEM TOOLS
     # -------------------------------------
-    abstract type Basis end
-    export PolynomialBasis
+    export FEMBasis
 
     export P1IntLegendreGenerator
 
@@ -67,6 +62,9 @@ module AtomicKohnSham
     @inline getpolynomial(gen::AbstractGenerator, n::Int) = gen[n]
     @inline getderivpolynomial(gen::AbstractGenerator, n::Int) = getderivpolynomial(gen)[n]
 
+    export Mesh, linmesh, geometricmesh, polynomialmesh, expmesh
+    include("fem/mesh.jl")
+
     include("fem/generators.jl")
     include("fem/basis.jl")
     include("fem/computations.jl")
@@ -76,65 +74,56 @@ module AtomicKohnSham
 
 
     # -------------------------------------
-    #               PROBLEM
+    #               MODELISATION
     # -------------------------------------
-    # KOHN-SHAM MODEL
-    export ExchangeCorrelation,NoExchangeCorrelation, SlaterXα, LSDA
-    export exc, vxc, vxcUP, vxcDOWN
-    export KohnShamExtended, ReducedHartreeFock
+    export ExchangeCorrelation, NoExchangeCorrelation, SlaterXα, LSDA
+    export KSEModel, RHF, SlaterXα
+
+    include("exchange correlation.jl")
     include("models.jl")
     
+
     # -------------------------------------
-    #               1D FEM TOOLS
+    #               DISCRETIZATION
     # -------------------------------------
-    abstract type KohnShamDiscretization end
+    export KSEDiscretization
+    include("discretization/structure.jl")
+    include("discretization/operator.jl")
+    include("discretization/energies.jl")
+    include("discretization/density.jl")
 
-    abstract type SCFMethod end
-    abstract type SCFCache end
-    abstract type SCFSolution end
-
-
-
+    
+    # -------------------------------------
+    #               LogBook
+    # -------------------------------------
     export LogConfig, LogBook
     include("log.jl")
-
-    export KohnShamSolver, SolverOptions
-    include("solver.jl")
-
-
-    loopheader!(::SCFCache, ::SCFMethod, ::KohnShamSolver)      = nothing
-    performstep!(::SCFCache, m::SCFMethod, ::KohnShamSolver)    = @warn "No performstep for the method $(typeof(m))"
-    loopfooter!(::SCFCache, ::SCFMethod, ::KohnShamSolver)      = nothing
-    monitor(::SCFCache, ::SCFMethod, ::KohnShamSolver)          = nothing
-    register!(::SCFCache, ::SCFMethod, ::KohnShamSolver)        = nothing
-    create_cache_method(m::SCFMethod, 
-                        ::KohnShamDiscretization)               = @warn "No creation of cache for the method $(typeof(m))"
-    switch!(c2::SCFCache, c1::SCFCache)                         = @warn "No way to init $(typeof(c2)) from of cache for the method ($(typeof(c1))"
-    
-    # DISCRETIZATION
-    export LDADiscretization, LSDADiscretization
-    include("discretization/lda.jl")
-    include("discretization/lsda.jl")
-
-    export DFTProblem
-    include("problem.jl")
 
     # -------------------------------------
     #            SCF ALGORITHMS
     # -------------------------------------
-    export CDA, ODA, Quadratic
-    include("methods/rca.jl")
-    include("methods/oda_procedure.jl")
-    #include("methods/quadratic.jl")
-    include("methods/combined.jl")
+    export KSESolver, SolverOptions
+    export SCFAlgorithm, CDA, ODA, Quadratic
+
+    include("algorithms/abstract.jl")
+
+    include("solver.jl")
+
+    include("algorithms/rca.jl")
+    include("algorithms/oda_procedure.jl")
+    #include("algorithms/quadratic.jl")
+    include("algorithms/combined.jl")
 
     export aufbau!
-    include("aufbau.jl")
+    include("algorithms/aufbau.jl")
 
     # -------------------------------------
-    #               1D FEM TOOLS
+    #       PROBLEM-groundstate-SOLUTION
     # -------------------------------------
-    export KohnShamSolution, eigenvector, density, total_charge
+    export AtomProblem
+    include("problem.jl")
+
+    export KSESolution, eigenvector, density, total_charge
     include("solution.jl")
 
     export groundstate
