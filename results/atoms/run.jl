e3e956840003e7ef6fbdec00e4d88fdc11b0a795
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-06-03 13:08:19 +0200

    todo

--- results/atoms/run.jl
+++ results/atoms/run.jl
@@ -10,16 +10,16 @@ logconfig = LogConfig(orbitals_energy = true, occupation_number = true, energy =
 
 problem = AtomProblem(;
                 T               = Float64, 
-                lh              = 0,
-                nh              = 1, 
+                lh              = 1,
+                nh              = 2, 
                 alg             = ODA(0.4), 
-                model           = RHF(;z=1, N=1),  
+                model           = RHF(;z=8, N=8),  
                 Rmax            = 100, 
                 Nmesh           = 10,
                 typemesh        = expmesh, 
                 optsmesh        = (s = 1.5,), 
                 typebasis       = P1IntLegendreGenerator, 
-                optsbasis       = (ordermax = 20,), 
+                optsbasis       = (ordermax = 10,), 
                 name            = "Hydrogen", 
                 scftol          = 1e-11,
                 maxiter         = 60,
@@ -28,6 +28,6 @@ problem = AtomProblem(;
                 verbose         = 0)
 
 # RESOLUTION
-sol = groundstate(problem)
+@time sol = groundstate(problem);
 
 plot_stopping_criteria([sol]) 
