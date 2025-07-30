6879ddb8023c8453d684fa858168b1a3a3465009
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-07-30 14:06:04 +0200

    deploy doc

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,7 @@ push!(LOAD_PATH, "../src/")
 using Documenter, AtomicKohnSham
 
 makedocs(sitename = "AtomicKohnSham Documentation")
+
+deploydocs(
+    repo = "github.com/Theozeud/AtomicKohnSham.git",
+)
