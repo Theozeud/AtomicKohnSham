28bb6ebc49427b58d346708c375898be0a836699
Théo Duez <42682941+Theozeud@users.noreply.github.com>
2025-07-30 15:47:37 +0200

    add file to doc

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,16 @@ push!(LOAD_PATH, "../src/")
 
 using Documenter, AtomicKohnSham
 
-makedocs(sitename = "AtomicKohnSham Documentation")
+makedocs(sitename = "AtomicKohnSham Documentation",
+    pages = [
+        "index.md",
+        "Tutorials" => [
+            "Models" => "tutorials/model.jl",
+            "AtomProblem" => "tutorials/problem.jl",
+            "Groundstate" => "tutorials/groundstate.jl",
+            "Analysing results" => "tutorials/solution.jl"
+        ]
+    ])
 
 deploydocs(
     repo = "github.com/Theozeud/AtomicKohnSham.git",
